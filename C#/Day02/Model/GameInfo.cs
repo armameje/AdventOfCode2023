94f9acf43718a912f0731c6610bc32231d24714e
armameje <armamentojenisi@gmail.com>
2023-12-08 05:13:52 +0800

    Add Day02 Part01 C#; restructured C# files

---
+++ C#/Day02/Model/GameInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Day02.Model
+{
+    internal class GameInfo
+    {
+        public GameInfo()
+        {
+            
+        }
+        public GameInfo(int gameNumber, List<GameSetInfo> gameSets, bool gameEligible)
+        {
+            GameNumber = gameNumber;
+            GameSets = gameSets;
+            IsGameEligible = gameEligible;
+        }
+
+        public int GameNumber { get; init; }
+        public List<GameSetInfo> GameSets { get; init; }
+        public bool IsGameEligible { get; init; }
+    }
+}
---
+++ C#/Day02/Model/GameSetInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Day02.Model
+{
+    internal class GameSetInfo
+    {
+        public GameSetInfo()
+        {
+
+        }
+
+        public GameSetInfo(int red, int green, int blue)
+        {
+            RedCube = red;
+            GreenCube = green;
+            BlueCube = blue;
+        }
+
+        public int RedCube { get; set; }
+        public int GreenCube { get; set; }
+        public int BlueCube { get; set; }
+    }
+}
